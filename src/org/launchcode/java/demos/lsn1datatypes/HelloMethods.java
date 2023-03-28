475daf21651a72f5f6507ec68be42eb6f14bd329
corty2987 <cortyblackwell@gmail.com>
2023-03-27 19:47:27 -0500

    Committing all the things

--- src/org/launchcode/java/demos/lsn1datatypes/HelloMethods.java
+++ src/org/launchcode/java/demos/lsn1datatypes/HelloMethods.java
@@ -2,9 +2,11 @@ package org.launchcode.java.demos.lsn1datatypes;
 
 public class HelloMethods {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //psvm
         String message = Message.getMessage("gib");
         System.out.println(message);
+        //sout tab!!!!
+
     }
 
 }
