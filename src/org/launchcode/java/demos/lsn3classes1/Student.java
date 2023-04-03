22d79b2402701ea4cea004d10ab38549b11bd9e6
corty2987 <cortyblackwell@gmail.com>
2023-04-03 13:48:22 -0500

    Committing exercises

--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -4,10 +4,53 @@ package org.launchcode.java.demos.lsn3classes1;
 // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
 
 public class Student {
-
     private String name;
     private int studentId;
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
+    //add getters and setters
+    //instantiate the Student Class
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    private void setNumberOfCredits(int numberOfCredits) {
+        this.numberOfCredits = numberOfCredits;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public Student(String name, int studentId, int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+    public Student(String name, int studentId) {
+        this("Courtney", 12345, 1, 4);
+    }
 }
