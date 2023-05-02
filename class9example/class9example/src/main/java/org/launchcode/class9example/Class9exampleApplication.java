fdef85a3c7b207bc48d08228d08584947e3d2e6f
corty2987 <cortyblackwell@gmail.com>
2023-05-02 14:21:36 -0500

    Added a form

---
+++ class9example/class9example/src/main/java/org/launchcode/class9example/Class9exampleApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.class9example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Class9exampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Class9exampleApplication.class, args);
+	}
+
+}
---
+++ class9example/class9example/src/test/java/org/launchcode/class9example/Class9exampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.class9example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Class9exampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
